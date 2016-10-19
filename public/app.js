2c91b20f2592a2b5994d5de8cf5970df53f86bbe
Anders Pedersen <anders@opeepl.com>
2016-10-19 22:11:35 +0200

    ini

--- public/app.js
+++ public/app.js
@@ -3,37 +3,232 @@
 var app = angular.module('myApp', ['ngRoute']);
 
 
-app.config(function ($routeProvider) {
+app.config(function ($routeProvider, $locationProvider) {
+
 	// Routing
 	$routeProvider.
 	when('/', {
 		templateUrl: 'views/forside.html',
-		controller: 'mainController'
+		controller: 'frontpageController'
 	}).
-	
 	when('/omOs', {
 		templateUrl: 'views/omOs.html',
-		controller: 'mainController'
+		controller: 'aboutUsController'
 	}).
-	when('/tidligereArbejde', {
-		templateUrl: 'views/tidligereArbejde.html',
-		controller: 'mainController'
+	when('/billeder', {
+		templateUrl: 'views/billeder.html',
+		controller: 'imageController'
 	}).
 	when('/prisUdregning', {
 		templateUrl: 'views/prisUdregning.html',
-		controller: 'mainController'
+		controller: 'priceController'
 	}).
 	when('/kontakt', {
 		templateUrl: 'views/kontakt.html',
 		controller: 'mainController'
 	}).
+	when('/admin', {
+		templateUrl: 'views/admin.html',
+		controller: 'adminController'
+	}).
+	when('/adminLogin', {
+		templateUrl: 'views/adminLogin.html',
+		controller: 'adminLoginController.html'
+	}).
 	otherwise({
 		redirectTo: '/'
 	});
-	
+
+
+//TODO:  SET BASE 
+/*
+	$locationProvider.html5Mode({
+  	enabled: true
+	});
+	*/
+});
+
+// TODO MOVE TO OWN FOLDER
+// CONTROLLERS
+
+// MAIN CONTROLLER 
+app.controller('mainController', function($scope, $http) {
+	console.log('hello from main controller');
+
+	//get contact info
+
 });
 
 
-app.controller('mainController', function($scope, $http) {
-	console.log('hello from angular controller');
+// FRONTPAGE CONTROLLER 
+app.controller('frontpageController', function($scope, $http) {
+	console.log('hello from frontpage controller');
+
+	$http({
+		method: 'get',
+		url: '/api/getFrontpageTxt'
+	}).then(function succesCallback(response) {
+		$scope.frontpageTxt = response.data;
+
+	}, function errorCallback(response) {
+		console.log(response);
+	});
+
+});
+
+
+// ABOUT US CONTROLLER 
+app.controller('aboutUsController', function($scope, $http) {
+	console.log('hello from main controller');
+
+	$http({
+		method: 'get',
+		url: '/api/getAboutUsTxt'
+	}).then(function succesCallback(response) {
+		$scope.aboutUsText = response.data;
+
+	}, function errorCallback(response) {
+		console.log(response);
+	})
+
+});
+
+
+// PRICE CALC. CONTROLLER
+app.controller('priceController', function($scope, $http) {
+	console.log('hello from price controller')
+
+	// ng-models for clients input (house width and length)
+	$scope.houseWidth = "";
+  $scope.houseLength = "";
+	
+	// initate starting price at zero
+	$scope.currentPrice = 0;
+
+	// get prices
+	$http({
+		method: 'get',
+		url: '/api/getPrices'
+	}).then(function succesCallback(response) {
+		$scope.prices = response.data;
+
+	}), function errorCallback(response) {
+		console.log(response);
+	}
+
+	// Calculate clients price.
+	$scope.calcPrice = function() {
+		var lengthPrice = $scope.prices.lengthPrice;
+		var widthPrice = $scope.prices.widthPrice;
+		
+		$scope.currentPrice = +($scope.houseLength * lengthPrice) + +($scope.houseWidth * widthPrice);
+
+	}
+});
+
+
+// IMAGE CONTROLLER
+app.controller('imageController', function($scope, $http) {
+	console.log('hello from image controller');
+
+	$scope.imageSrcArray = [];
+
+	$http({
+		method: 'get',
+		url: '/api/getImages'
+	}).then(function succesCallback(response) {
+		var data = response.data;
+		
+			// make nested array for nested ng-repeat for bootstrap rows / cols.
+			while(data.length > 0) {
+				var	chunk = data.splice(0,3);
+				$scope.imageSrcArray.push(chunk);
+			} 
+		}, function errorCallback(response) {
+			console.log(response);
+		});
+
+
+});
+
+
+// LOGIN CONTROLLER
+app.controller('loginController', function($scope, $http) {
+	console.log('hello from login controller');
+});
+
+
+// ADMIN CONTROLLER
+app.controller('adminController', function($scope, $http, $location) {	
+	console.log('admin controller active');
+	
+	// authCheck
+	var loggedIn = true;
+	if(!loggedIn) {
+		$location.path("/");
+	}
+
+	//ng-model to edit frontpage textarea
+	$scope.frontPageTextModel = "";
+  $scope.aboutUsTextModel = "";
+
+  //ng-model for prices
+  $scope.widthPriceModel = "";
+  $scope.lengthPriceModel = "";
+
+  //ng model for contact info
+  $scope.emailModel = "";
+  $scope.phoneNumberModel = "";
+
+
+	// update frontpage text
+	$scope.updateFrontpageTxt = function() {
+
+		var textToUpdate = {
+			text: $scope.frontPageTextModel
+		};
+
+		// todo - catch errors
+		$http.post('/api/updateFrontpageTxt', textToUpdate).then(function(succes) {
+			console.log('updated frontpagetext succesfully!');
+		});
+	};
+
+	// update about us text
+	$scope.updateAboutUsTxt = function() {
+
+		var textToUpdate = {
+			text: $scope.aboutUsTextModel
+		}
+
+		$http.post('/api/updateAboutUsTxt', textToUpdate).then(function(succes) {
+			console.log('updated aboutUstext succesfully!')
+		});
+	};
+	
+	// update kontakt info
+	$scope.updateContactInfo = function() {
+		var newContactInfo = {
+			email: $scope.emailModel,
+			number: $scope.phoneNumberModel
+		};
+
+		$http.post('/api/updateContactInfo', newContactInfo).then(function(succes) {
+			console.log('updated contactinfo succesfully');	
+		})
+
+	};
+
+	// update prices
+	$scope.updatePrices = function() {
+		var newPrices = {
+			widthPrice: $scope.widthPriceModel,
+			lengthPrice: $scope.lengthPriceModel
+		}
+
+		$http.post('/api/updatePrices', newPrices).then(function(succes) {
+			console.log('updated prices succesfully')
+		})
+	}
+
 });
--- server.js
+++ server.js
@@ -1,10 +1,153 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var fs = require('fs');
+var multer  = require('multer');
+var upload = multer({ dest: 'public/images/' });
 
 var app = express();
 
+
+// db connection - using raw txt files instead. 
+//mongoose.connect('mongodb://drajz:hej@ds041546.mlab.com:41546/nataek');
+
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-	console.log('Server listening on port 3000...');
+// support JSON-encoded bodies
+app.use(bodyParser.json());
+
+app.listen(7000, function() {
+	console.log('Server listening on port 7000...');
+});
+
+
+// API ROUTES
+
+// get frontpage text
+app.get('/api/getFrontpageTxt', function(req, res) {
+	fs.readFile('server/data/forsideTekst.txt', 'utf8', function (err, data) {
+		if (err) {
+			return console.log(err);
+		}
+
+		res.send(data);
+	});
+
+});
+
+// Update frontpage text
+app.post('/api/updateFrontPageTxt', function(req, res) {
+
+	var newText = req.body.text;
+	fs.writeFile('server/data/forsideTekst.txt', newText, function() {
+		console.log('updated forsideTekst.txt successfully!');
+	})
+	
+	res.end('{"success" : "Updated successfully", "status" : 200}');
 })
+
+// Get about us text
+app.get('/api/getAboutUsTxt', function(req, res) {
+	fs.readFile('server/data/omOsTekst.txt', 'utf8', function (err, data) {
+		if (err) {
+			return console.log(err);
+		}
+
+		res.send(data);
+	});
+
+})
+
+// Update about us text 
+app.post('/api/updateAboutUsTxt', function(req, res) {
+
+	var newText = req.body.text;
+	fs.writeFile('server/data/omOsTekst.txt', newText, function() {
+		console.log('update about omOsTekst.txt successfully')
+	})
+
+	res.end('{"success" : "Updated successfully", "status" : 200}');	
+});
+
+// Get contact info
+app.get('/api/getContactInfo', function(req, res) {
+	fs.readFile('server/data/kontaktInfo.txt', 'utf8', function(err, data) {
+		if (err) {
+			return console.log(err);
+		}
+		// format email,number
+		var dataOpdelt = data.split(',');
+
+		var contactInfo = {
+			email : dataOpdelt[0],
+			number : dataOpdelt[1]
+		};
+
+		res.send(contactInfo);
+	})
+})
+
+// Update contact info
+// format : email,number
+app.post('/api/updateContactInfo', function(req, res) {
+
+	var newContactInfo = req.body.email + "," + req.body.number;
+	fs.writeFile('server/data/kontaktInfo.txt', newContactInfo, function() {
+		console.log('updated kontaktInfo.txt successfully')
+		res.end('{"success" : "Updated successfully", "status" : 200}');
+
+	})
+})
+
+
+
+// Get prices
+// format : 'width,length'
+app.get('/api/getPrices', function(req, res) {
+	fs.readFile('server/data/priser.txt', 'utf8', function (err, data) {
+		if (err) {
+			return console.log(err);
+		}
+		
+		var dataOpdelt = data.split(',');
+
+		var prices = {
+			lengthPrice : parseInt(dataOpdelt[0]),
+			widthPrice :  parseInt(dataOpdelt[1])
+		};
+
+		res.send(prices);
+	})
+})
+
+// Update prices
+app.post('/api/updatePrices', function(req, res) {
+
+	var newPrices = req.body.widthPrice + "," + req.body.lengthPrice;
+	fs.writeFile('server/data/priser.txt', newPrices, function() {
+		console.log('updated priser.txt succesfully')
+	})
+
+});
+
+// Get images
+app.get('/api/getImages', function(req, res) {
+	var files = fs.readdirSync('public/images');
+	
+	res.send(files);
+});
+
+// Upload image.
+app.post('/api/upload', upload.any(), function(req, res) {
+	console.log('new image was uploaded: ');
+	console.log(req.files);
+
+	res.end('{"success" : "uploaded image Successfully", "status" : 200}');
+});
+
+// Delete image.
+
+// Login Auth.
+app.get('/api/', function(req, res) {
+	// todo
+});
